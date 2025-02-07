7dc243dc232d29ead60e4aa72d8f366127e33e0c
adilq <adil.quettawala@mail.utoronto.ca>
2025-02-07 10:06:36 -0500

    redirect pose info from vicon to mavros

---
+++ drone_ws/src/vicon/vicon/redirect_pose_info.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+
+class Redirector(Node):
+	def __init__(self):
+		super().__init__('redirector_node')
+		self.publisher_ = self.create_publisher(PoseStamped, '/mavros/vision_pose/pose', 10)
+		self.subscriber_ = self.create_subscription(PoseStamped, '/vicon/ROB498_Drone/ROB498_Drone', self.pose_callback, 10)
+		self.subscriber_
+
+	def pose_callback(self, msg):
+		self.publisher_.publish(msg)
+	
+def main(args=None):
+	rclpy.init(args=args)
+	redirector = Redirector()
+	rclpy.spin(redirector)
+
+if  __name__ == '__main__':
+	main()
+
