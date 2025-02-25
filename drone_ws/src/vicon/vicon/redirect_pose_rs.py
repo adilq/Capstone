d45928589b000e0e19aea96d876d65335fa33187
Adil Q <adil.quettawala@mail.utoronto.ca>
2025-02-25 15:54:14 -0500

    add redirector for realsense

---
+++ drone_ws/src/vicon/vicon/redirect_pose_rs.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+
+class Redirector_rs(Node):
+	def __init__(self):
+		super().__init__('redirector_node_rs')
+		self.publisher_ = self.create_publisher(PoseStamped, '/mavros/vision_pose/pose', 10)
+		self.subscriber_ = self.create_subscription(PoseStamped, '/camera/pose/sample', self.pose_callback, 10)
+		self.subscriber_
+
+	def pose_callback(self, msg):
+		out_msg = PoseStamped()
+		out_msg.header.frame_id = 'map'
+		out_msg.pose = msg.pose.pose
+		self.publisher_.publish(out_msg)
+	
+def main(args=None):
+	rclpy.init(args=args)
+	redirector = Redirector_rs()
+	rclpy.spin(redirector)
+
+if  __name__ == '__main__':
+	main()
