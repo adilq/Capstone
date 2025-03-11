b23d7a9edd17790959510d9038867d72d8bac0d6
Ria Upreti <riaupreti1@gmail.com>
2025-03-11 11:22:54 -0400

    minor changes to waypoint publisher node.

--- drone_ws/src/offboard_control/offboard_control/waypoint_node.py
+++ drone_ws/src/offboard_control/offboard_control/waypoint_node.py
@@ -3,19 +3,27 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseArray, Pose
 
 # TO-DO: set up waypoints as a parameter we can modify from the command line
+p1 = Pose()
+p2 = Pose()
+p3 = Pose()
+p4 = Pose()
+waypoints = PoseArray()
+waypoints.poses = [p1, p2, p3, p4]
 
 class WaypointNode(Node):
     def __init__(self):
         super().__init__('rob498_drone_8')
         self.waypoints_pub = self.create_publisher(PoseArray, 'rob498_drone_8/comm/waypoints', 10)
-
-        self.rate = self.create_rate(30)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+        
+    def timer_callback(self):
+        self.waypoints_pub.publish(waypoints)
 	
 def main(args=None):
 	rclpy.init(args=args)
 	node = WaypointNode()
-	node.waypoints_pub.publish(WAYPOINTS)
 	rclpy.spin(node)
+	rclpy.shutdown()
 
 if  __name__ == '__main__':
 	main()
