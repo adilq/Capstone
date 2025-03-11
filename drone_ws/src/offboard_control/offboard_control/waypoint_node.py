098635a348c0802668ee4760d8755822626dbe7f
Ria Upreti <riaupreti1@gmail.com>
2025-03-11 12:14:45 -0400

    waypoints added

--- drone_ws/src/offboard_control/offboard_control/waypoint_node.py
+++ drone_ws/src/offboard_control/offboard_control/waypoint_node.py
@@ -1,23 +1,59 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseArray, Pose
+from geometry_msgs.msg import PoseArray, Pose, Point
 
 # TO-DO: set up waypoints as a parameter we can modify from the command line
 p1 = Pose()
+p1.pose.position = Point(x=1.8, y=1.9, z=0.8)
+# p1.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+# 1781 1884 893
+
 p2 = Pose()
+p2.pose.position = Point(x=1.8, y=-1.8, z=0.8)
+# p2.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+# 1765 -1778 897
+
 p3 = Pose()
+p3.pose.position = Point(x=-1.8, y=-1.6, z=0.9)
+# p3.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+# -1785 -1607 941
+
 p4 = Pose()
-waypoints = PoseArray()
-waypoints.poses = [p1, p2, p3, p4]
+p4.pose.position = Point(x=-1.7, y=2.0, z=0.9)
+# p4.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+# -1679 1961 931
 
 class WaypointNode(Node):
     def __init__(self):
         super().__init__('rob498_drone_8')
         self.waypoints_pub = self.create_publisher(PoseArray, 'rob498_drone_8/comm/waypoints', 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
+        self.waypoints = PoseArray()
+        p1 = Pose()
+        p1.pose.position.x = 5
+        p1.pose.position.y =5
+        p1.pose.position.z = 5
+        p1.pose.position = Point(x=1.8, y=1.9, z=0.8)
+        # p1.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+        # 1781 1884 893
+
+        p2 = Pose()
+        p2.pose.position = Point(x=1.8, y=-1.8, z=0.8)
+        # p2.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+        # 1765 -1778 897
+
+        p3 = Pose()
+        p3.pose.position = Point(x=-1.8, y=-1.6, z=0.9)
+        # p3.pose.orientation = Quaternion(x=0., y=0., z=0., w=1.)
+        # -1785 -1607 941
+
+        p4 = Pose()
+        p4.pose.position = Point(x=-1.7, y=2.0, z=0.9)
+        for pose in [p1, p2, p3, p4]:
+            self.waypoints.poses.append(pose)
         
     def timer_callback(self):
-        self.waypoints_pub.publish(waypoints)
+        self.waypoints_pub.publish(self.waypoints)
 	
 def main(args=None):
 	rclpy.init(args=args)
