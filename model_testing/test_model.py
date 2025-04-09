25bd7bbe8a22118ed3197bd2c424df8adfe557d0
Adil Q <adil.quettawala@mail.utoronto.ca>
2025-04-09 11:00:59 -0400

    use camera for test

--- model_testing/test_model.py
+++ model_testing/test_model.py
@@ -1,15 +1,22 @@
 import cv2
 
 from ultralytics import YOLO
-import torch
+# import torch
 
-cv_image = cv2.imread('goodpic.jpg')
+# cv_image = cv2.imread('goodpic.jpg')
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)480,format=(string)NV12, framerate=(fraction)30/1 ! \
+    nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink", cv2.CAP_GSTREAMER)
+
+ret, cv_image = cap.read()
+if not ret:
+    print('Failed to capture image from /dev/video0')
+
 model = YOLO('best.pt')
 model.eval()
 result = model(cv_image)
 
 boxes_xyxy =  result[0].boxes.xyxy
-print(boxes_xyxy[:, 0])
+print(list(boxes_xyxy[:, 0]))
 
 # boxes_cls =  torch.Tensor.numpy(result[0].boxes.cls)
 # boxes_conf = torch.Tensor.numpy(result[0].boxes.conf)
